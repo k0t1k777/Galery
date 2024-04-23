b8712d661de77117be8d6e2a48914bf52a86b69e
Nickolai <lindeman072@mail.ru>
2024-04-23 17:49:35 +0400

    add: cards

--- src/components/Main/Gallery/Gallery.tsx
+++ src/components/Main/Gallery/Gallery.tsx
@@ -3,33 +3,12 @@ import './Gallery.css';
 
 export default function Gallery() {
   return (
-    // <div className='gallery'>
-    //   <Picture />
-    //   <Picture />
-    //   <Picture />
-    //   <Picture />
-    //   <Picture />
-    //   <Picture />
-    // </div>
-    <ul className='gallery'>
-      <li className='gallery__item'>
-        <Picture />
-      </li>
-      <li className='gallery__item'>
-        <Picture />
-      </li>
-      <li className='gallery__item'>
-        <Picture />
-      </li>
-      <li className='gallery__item'>
-        <Picture />
-      </li>
-      <li className='gallery__item'>
-        <Picture />
-      </li>
-      <li className='gallery__item'>
-        <Picture />
-      </li>
-    </ul>
+    <div className='gallery'>
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+    </div>
   );
 }
